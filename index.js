54873aa4ca0f0f25a4dbff9d5e99745cf26b0dd9
Grant Register <register.grant@gmail.com>
2022-09-12 18:49:27 -0600

    added a few prompts

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const db = mysql.createConnection(
     {
-      //host: "localhost",
+      host: "localhost",
       user: "root",
       password: process.env.DB_PASSWORD,
       database: "trapperKeeper_db",
@@ -24,21 +24,21 @@ const startApp = () => {
     return inquirer.prompt([{
         name: 'action',
         type: 'list',
-        message: 'What would you like to do?',
-        choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role"]
+        message: 'Make your choice. Choose wisely.',
+        choices: ["View Departments", "View Roles", "View Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role"]
     }]).then(function(val) {
         switch (val.action) {
-            case "View All Departments":
+            case "View Departments":
                 console.log(val.action);
-                viewAllDepartments();
+                viewDepartments();
                 break;
 
-            case "View All Roles":
-                viewAllRoles();
+            case "View Roles":
+                viewRoles();
                 break;
 
-            case "View All Employees":
-                viewAllEmployees();
+            case "View Employees":
+                viewEmployees();
                 break;
 
             case "Add Department":
@@ -58,3 +58,67 @@ const startApp = () => {
         }
     });
 }
+
+const viewDepartments = () => {
+    (function(err, res) {
+        if (err) {
+            console.log('You screwed up...');
+            console.dir(err);
+            return;
+        }
+        console.log(res)
+    })
+    const query = `SELECT * FROM departments`;
+    db.query(query,
+        function(err, res) {
+            if (err) throw err
+            console.table(res)
+            startApp()
+        })
+};
+
+const viewRoles = () => {
+    (function(err, res) {
+        if (err) {
+            console.log('You screwed up AGAIN...');
+            console.dir(err);
+            return;
+        }
+        console.log(res)
+    })
+    const query = `SELECT roles.id, roles.title, roles.salary, roles.department_id FROM roles`;
+    db.query(query,
+        function(err, res) {
+            if (err) throw err
+            console.table(res)
+            startApp()
+        })
+};
+
+const viewEmployees = () => {
+    (function(err, res) {
+        if (err) {
+            console.log('You are bad at this...');
+            console.dir(err);
+            return;
+        }
+        console.log(res)
+    })
+    const query = `SELECT employees.id, employees.first_name, employees.last_name, departments.name, roles.title, roles.salary, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employees INNER JOIN roles on roles.id = employees.role_id INNER JOIN departments on departments.id = roles.department_id left join employees e on employees.manager_id = e.id;`;
+    db.query(query,
+        function(err, res) {
+            if (err) throw err
+            console.table(res)
+            startApp()
+        })
+};
+
+const addDepartment = () => {
+    (function(err, res) {
+        if (err) {
+            console.log('Seriously. Quite bad...');
+            console.dir(err);
+            return;
+        }
+        console.log(res)
+    })
